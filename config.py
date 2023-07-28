49da480af7140edec97fef88cee1712c7df48c83
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-07-28 14:17:23 +0530

    config.py
    Signed-off-by: KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/e602356bafe2799f708bd.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
