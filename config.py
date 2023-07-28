cd1b93fa6b71c97630719a8f951b3c265e228b6e
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-07-28 15:56:42 +0530

    config.py
    Signed-off-by: KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1138802391").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://graph.org/file/a7241f67a3ca3fc888f10.mp4")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/KinGEvilHacker/AnonXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
